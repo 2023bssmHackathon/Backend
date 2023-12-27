8413a284a9bfd77adb21350c4fa813ec10515dfa
hun1i <nicebrian0328@gmail.com>
2023-12-28 00:26:46 +0900

    ADD :: static path

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { BoardModule } from './board/board.module';
 import { NoteModule } from './note/note.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DatabaseModule, BoardModule, NoteModule],
+  imports: [
+    DatabaseModule,
+    BoardModule,
+    NoteModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
