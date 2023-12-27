927a9f6541db58221aa657ed99540a4bb04a9cd7
hun1i <nicebrian0328@gmail.com>
2023-12-27 15:37:01 +0900

    ADD :: database setting

---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+require('dotenv').config();
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB,
+      synchronize: true,
+      entities: [],
+    }),
+  ],
+})
+export class DatabaseModule {}
